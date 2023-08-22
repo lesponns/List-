4df6edcac8c60aa5035604c53e8ef71a6a6dd27b
lesponns <adamadakadam1903@gmail.com>
2023-08-22 19:04:37 +0300

    Add project files.

---
+++ SmartPro_36[22-08-23]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_36_22_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(10);
+            numbers.Add(34);
+            numbers.Add(63);
+            numbers.Add(2);
+            numbers.Add(-72);
+
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                Console.Write("{0}.Index Değeri: {1}\n",i,numbers[i]);
+            }
+            Console.Write("_________________________\n");
+
+            numbers.Remove(2);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write("{0}.Index Değeri: {1}\n", i, numbers[i]);
+            }
+            Console.Write("_________________________\n");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
